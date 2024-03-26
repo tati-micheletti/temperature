ca35114efa8733fd93a78ab227430c53ce276361
Tati Micheletti <tati.micheletti@gmail.com>
2024-03-26 17:23:24 +0100

    Version BUMP

--- temperature.R
+++ temperature.R
@@ -14,7 +14,7 @@ defineModule(sim, list(
                                 email = "tati.micheletti@gmail.com", comment = NULL)), 
                       class = "person"),
   childModules = character(0),
-  version = list(temperature = "0.0.0.9000"),
+  version = list(temperature = "1.0.0"),
   timeframe = as.POSIXlt(c(2013, 2032)),
   timeunit = "year",
   citation = list("citation.bib"),
